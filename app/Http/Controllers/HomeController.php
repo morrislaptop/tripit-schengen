8738960d41bb2e4c75f97f2cd6434500d5fa4f6d
Craig Morris <craig.michael.morris@gmail.com>
2018-06-06 13:30:36 +0100

    WIP

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use TripIt;
+use DateInterval;
+use DateTimeImmutable;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use OAuthConsumerCredential;
+use Illuminate\Support\Facades\Cache;
 use Laravel\Socialite\Facades\Socialite;
 use League\OAuth1\Client\Credentials\CredentialsException;
 
@@ -43,6 +49,146 @@ class HomeController extends Controller
     public function calculate(Request $request) {
         $user = $request->session()->get('user');
 
-        dump($user);
+        if (!$user) return redirect('/login');
+
+        $creds = new OAuthConsumerCredential(
+            config('services.tripit.key'),
+            config('services.tripit.secret'),
+            $user->token,
+            $user->tokenSecret
+        );
+
+        $tripit = new TripIt($creds);
+
+        $past = $this->getTrips($tripit, true);
+        $future = $this->getTrips($tripit, false);
+
+        // Fix not being an array if only one record.
+        $future['Trip'] = empty($future['Trip'][0]) ? [$future['Trip']] : $future['Trip'];
+
+        $trips = collect(array_merge($past['Trip'], $future['Trip']))->sortBy('start_date');
+
+        $now = $request->date ? new DateTimeImmutable($request->date) : new DateTimeImmutable();
+        $since = $now->sub(new DateInterval('P180D'));
+
+        $trips = $trips->filter(function ($trip) use ($since, $now) {
+            $end = new DateTimeImmutable($trip['end_date']);
+            $start = new DateTimeImmutable($trip['start_date']);
+
+            return $end > $since || $start > $now;
+        })->values();
+
+        $debug = [];
+
+        $in = $trips->reduce(function ($carry, $trip) use ($since, $now, &$debug) {
+            $end = new DateTimeImmutable($trip['end_date']);
+            $start = new DateTimeImmutable($trip['start_date']);
+
+            // boundaries
+            $start = max($since, $start); // if we were in schengen before 180 days ago, only count from 180 days ago
+            $end = min($end, $now); // only count up to today...
+
+            // calculate the days
+            $days = $end->diff($start)->days;
+
+            // if it wasn't in a Schengen country, don't count it.
+            $diff = $this->inSchengen($trip) ? $days : $days * -1;
+
+            $used = $carry + $diff;
+
+            $used = max(0, $used); // can never use negative days
+
+            // add debugging info
+            $debug[] = [
+                'start' => $start,
+                'end' => $end,
+                'days' => $days,
+                'diff' => $carry - $used,
+            ];
+
+            return $used;
+        }, 0);
+
+        // Add the days since they've been back (if end date is before $now)
+        $waiting = 0;
+        $returned = new DateTimeImmutable($trips->sortBy('end_date')->last()['end_date']);
+        if ($returned < $now) {
+            $waiting = $now->diff($returned)->days;
+        }
+
+        // Render the view
+        $log = $this->getTripLog($trips, $debug);
+        $remaining = 90 - $in + $waiting;
+        return view('calculate', compact('log', 'remaining', 'in', 'now', 'waiting'));
+    }
+
+    protected function getTripLog($trips, $debug)
+    {
+        $used = 0;
+        $remaining = 90;
+        $arr = [];
+
+        foreach ($trips as $i => $trip) {
+            $diff = $debug[$i]['diff'];
+
+            $arr[] = [
+                'country' => $trip['primary_location'],
+                'arrive' => $debug[$i]['start'],
+                'leave' => $debug[$i]['end'],
+                'days' => $debug[$i]['days'],
+                'diff' => $debug[$i]['diff'],
+                'used' => $used -= $diff,
+                'remaining' => $remaining += $diff,
+            ];
+        }
+
+        return $arr;
+    }
+
+    protected function getTrips($tripit, $past) {
+        $args = [
+            'past' => $past ? 'true' : 'false',
+            'format' => 'json',
+            'page_size' => 100,
+        ];
+        $trips = $tripit->list_trip($args);
+
+        return $trips;
+    }
+
+    protected function inSchengen($trip)
+    {
+        $cc = $trip['PrimaryLocationAddress']['country'];
+
+        $schengens = [
+            'BE',
+            'CZ',
+            'DK',
+            'DE',
+            'EE',
+            'GR',
+            'ES',
+            'FR',
+            'IT',
+            'LV',
+            'LT',
+            'LU',
+            'HU',
+            'MT',
+            'NL',
+            'AT',
+            'PL',
+            'PT',
+            'SI',
+            'SK',
+            'FI',
+            'SE',
+            'IS',
+            'NO',
+            'CH',
+            'AT',
+        ];
+
+        return in_array($cc, $schengens);
     }
 }
--- resources/views/calculate.blade.php
+++ resources/views/calculate.blade.php
@@ -1,23 +1,127 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+        <title>TripIt - Schengen</title>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-                    You are logged in!
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .subtitle {
+                font-size: 24px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    TripIt Schengen
                 </div>
+
+                <div class="subtitle m-b-md">
+                    You have {{ $remaining }} days remaining as of {{ $now->format('d M Y') }}
+                </div>
+
+                <form class="m-b-md">
+                    <input type="date" name="date" value="{{ $now->format('Y-m-d') }}" />
+                    <input type="submit" />
+                </form>
+
+                <div class="links m-b-md">
+                    <a href="/refresh">Refresh</a>
+                </div>
+
+                <table cellpadding="8">
+                    <tr>
+                        <th align="left">Country</th>
+                        <th>Arrive</th>
+                        <th>Leave</th>
+                        <th>Diff</th>
+                        <th>Remaining</th>
+                    </tr>
+                    <tr>
+                        <th>&nbsp;</th>
+                        <th>&nbsp;</th>
+                        <th>&nbsp;</th>
+                        <th>&nbsp;</th>
+                        <th>90</th>
+                    </tr>
+                    @foreach ($log as $trip)
+                        <tr>
+                            <td align="left">{{ $trip['country'] }}</td>
+                            <td>{{ $trip['arrive']->format('d M Y') }}</td>
+                            <td>{{ $trip['leave']->format('d M Y') }}</td>
+                            <td>{{ $trip['diff'] * -1 }}</td>
+                            <td>{{ $trip['remaining'] }}</td>
+                        </tr>
+                    @endforeach
+                    <tr>
+                        <td align="left"><em>Days Back</em></td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td><em>{{ $waiting }}</em></td>
+                        <td>{{ $remaining }}</td>
+                    </tr>
+                </table>
+
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    </body>
+</html>
